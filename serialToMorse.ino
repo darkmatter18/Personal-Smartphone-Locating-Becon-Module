eface2b9ff4cab844cfd0ceebe4eb898d8bd0960
Arkadip <in2arkadipb13@gmail.com>
2018-05-09 23:41:37 +0530

    LED responce added

--- serialToMorse.ino
+++ serialToMorse.ino
@@ -78,6 +78,29 @@ void loop(){
     }
     //Morse code Execution
     String morseWord = encode(arrReceiveMassage);
+
+    //Gives the LED responce
+    for(int i=0; i<=morseWord.length(); i++){
+        switch( morseWord[i] ){
+            case '.': //dit
+                digitalWrite( PIN_OUT, HIGH );
+                delay( UNIT_LENGTH );
+                digitalWrite( PIN_OUT, LOW );
+                delay( UNIT_LENGTH );
+                break;
+
+            case '-': //dah
+                digitalWrite( PIN_OUT, HIGH );
+                delay( UNIT_LENGTH*3 );
+                digitalWrite( PIN_OUT, LOW );
+                delay( UNIT_LENGTH );
+                break;
+
+            case ' ': //gap
+                delay( UNIT_LENGTH );
+        }
+    }
+}
 }
 
 //Function that handles all received bytes from phone
